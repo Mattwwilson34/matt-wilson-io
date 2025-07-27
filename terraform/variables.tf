ebe677a9a87a8cd034791c387c244acd5ba9dd59
mattradial <137439375+mattradial@users.noreply.github.com>
2025-07-27 11:26:31 -0400

    feat: add terraform vars

---
+++ terraform/variables.tf
@@ -0,0 +1,44 @@
+variable "do_token" {
+  description = "Digital Ocean API Token"
+  type        = string
+  sensitive   = true
+}
+
+variable "ssh_public_keys" {
+  description = "List of SSH public key paths"
+  type        = list(string)
+  default = [
+    "~/.ssh/digital_ocean_mattwilson_io_work.pub"
+  ]
+}
+
+variable "domain_name" {
+  description = "Your domain name"
+  type        = string
+  default     = "mattwilson.io"
+}
+
+variable "droplet_size" {
+  description = "Droplet size - cost controlled"
+  type        = string
+  default     = "s-1vcpu-1gb"
+
+  validation {
+    condition = contains([
+      "s-1vcpu-1gb", # $6/month
+    ], var.droplet_size)
+    error_message = "Only small droplet sizes allowed for cost control."
+  }
+}
+
+variable "region" {
+  description = "Digital Ocean region"
+  type        = string
+  default     = "nyc1"
+}
+
+variable "project_name" {
+  description = "Project name for resource naming"
+  type        = string
+  default     = "matt-wilson-io"
+}
